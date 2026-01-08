e47e7c5cde1aac65f47d557ae288c57eb911d6fa
vorraluck <vorraluck.t@kkumail.com>
2026-01-08 12:07:37 +0700

    lab4-1 fin commit

--- Lab4-1/cat.py
+++ Lab4-1/cat.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from datetime import datetime
 
 class Cat:
@@ -19,4 +20,27 @@ class Cat:
         print(f"Cat: {self.name} | Age: {self.age} | Owner: {self.owner}")
         print(f"Check-in: {self.date_in.strftime('%Y-%m-%d %H:%M')}")
         print(f"Check-out: {self.date_out.strftime('%Y-%m-%d %H:%M')}")
+=======
+from datetime import datetime
+
+class Cat:
+    
+    count = 0
+
+    def __init__(self, name, age, owner):
+        self.name = name
+        self.age = age
+        self.owner = owner
+        self.date_in = datetime.now()  
+        self.date_out = datetime.now() 
+        Cat.count += 1
+
+    def greet(self):
+        print(f"Meow! My name is {self.name}. Nice to meet you!")
+
+    def show_info(self):
+        print(f"Cat: {self.name} | Age: {self.age} | Owner: {self.owner}")
+        print(f"Check-in: {self.date_in.strftime('%Y-%m-%d %H:%M')}")
+        print(f"Check-out: {self.date_out.strftime('%Y-%m-%d %H:%M')}")
+>>>>>>> Stashed changes
         print("-" * 30)
